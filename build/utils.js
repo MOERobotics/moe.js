36fc172349a39c9a216482e16cb7b4b90c259231
mailmindlin <liamttlyrox@gmail.com>
2016-12-19 23:18:35 -0500

    Added ArrayBuffer#transfer polyfill

--- build/utils.js
+++ build/utils.js
@@ -1,4 +1,53 @@
 exports = {};
 function require() {
-        return exports;
+		return exports;
+}
+if(!ArrayBuffer.transfer) {
+	ArrayBuffer.transfer = function (source, dest) {
+		source = Object(source);
+		dest = Object(dest);
+		if(!(source instanceof ArrayBuffer) || !(dest instanceof ArrayBuffer)) {
+			throw new TypeError("Source and destination must be ArrayBuffer instances");
+		}
+		if(dest.byteLength >= source.byteLength) {
+			var nextOffset = 0;
+			var leftBytes = source.byteLength;
+			var wordSizes = [8, 4, 2, 1];
+			wordSizes.forEach(_wordSize_ => {
+				if (leftBytes >= _wordSize_) {
+					var done = transferWith(_wordSize_, source, dest, nextOffset, leftBytes);
+					nextOffset = done.nextOffset;
+					leftBytes = done.leftBytes;
+				}
+			});
+		}
+		function transferWith(wordSize, source, dest, nextOffset, leftBytes) {
+			var ViewClass = Uint8Array;
+			switch (wordSize) {
+				case 8:
+					ViewClass = Float64Array;
+					break;
+				case 4:
+					ViewClass = Float32Array;
+					break;
+				case 2:
+					ViewClass = Uint16Array;
+					break;
+				case 1:
+					ViewClass = Uint8Array;
+					break;
+				default:
+					ViewClass = Uint8Array;
+					break;
+			}
+			var view_source = new ViewClass(source, nextOffset, Math.trunc(leftBytes / wordSize));
+			var view_dest = new ViewClass(dest, nextOffset, Math.trunc(leftBytes / wordSize));
+			for(var i = 0; i < view_dest.length; i++)
+				view_dest[i] = view_source[i];
+			return {
+				nextOffset : view_source.byteOffset + view_source.byteLength,
+				leftBytes : source.byteLength - (view_source.byteOffset + view_source.byteLength)
+			}
+		}
+	};
 }
