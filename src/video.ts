2178707b55287a79830a8b727bb3b755752ebc13
mailmindlin <liamttlyrox@gmail.com>
2016-12-19 23:11:52 -0500

    Fixed MJPEGVideoStreamDecoder
    _encodeFrame was broken. It works now, but could be made faster... one
    of the few times that a native implementation of something in JS isn't
    the fastest.

--- src/video.ts
+++ src/video.ts
@@ -2,47 +2,38 @@ import {DataChannel, DataPacket, PacketRecievedEvent, PacketTypeCode} from "./ch
 export interface VideoStreamRenderer {
 	
 }
+export interface Rectangle {
+	top: number;
+	left: number;
+	width: number;
+	height: number;
+}
 export class MJPEGVideoStreamDecoder {
-        channel : DataChannel;
-        ctx : CanvasRenderingContext2D;
-        constructor(options : {channel:DataChannel, ctx : CanvasRenderingContext2D}) {
-                this.channel = options.channel;
-                this.ctx = options.ctx;
-                this.channel.addEventListener('packet', (e:PacketRecievedEvent)=>{
-                        if (e.packet.getTypeCode() !== PacketTypeCode.STREAM_FRAME)
-                                return;
-                        this._renderFrameMJPEG(e.packet);
-                });
-        }
-        _encodeFrame(data : Uint8Array) : string {
-                //Thanks to stackoverflow.com/a/11092371/2759984
-                var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-                var output = "";
-                var i : number = 0;
-
-                while (i < data.length) {
-                        var chr1 = data[i++];
-                        var chr2 = i < data.length ? data[i++] : Number.NaN; // Not sure if the index
-                        var chr3 = i < data.length ? data[i++] : Number.NaN; // checks are needed here
-
-                        var enc1 = chr1 >> 2;
-                        var enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-                        var enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-                        var enc4 = chr3 & 63;
-
-                        if (isNaN(chr2))
-                                enc3 = enc4 = 64;
-                        else if (isNaN(chr3))
-                                enc4 = 64;
-                        output += keyStr.charAt(enc1) + keyStr.charAt(enc2) + keyStr.charAt(enc3) + keyStr.charAt(enc4);
-                }
-                return "data:image/jpg,base64;" + output;
-        }
-        _renderFrameMJPEG(packet : DataPacket) : void {
-                var image = new Image();
-                image.onload = (e)=> {
-                        this.ctx.drawImage(image, 0, 0);
-                };
-                image.src = this._encodeFrame(new Uint8Array(packet.getArrayBuffer(), 16));
-        }
+	protected readonly channel : DataChannel;
+	protected readonly ctx : CanvasRenderingContext2D;
+	protected readonly rect : Rectangle;
+	constructor(options: {channel: DataChannel, ctx: CanvasRenderingContext2D, rect?: Rectangle}) {
+		this.channel = options.channel;
+		this.ctx = options.ctx;
+		this.rect = options.rect || {top: 0, left: 0, width: this.ctx.canvas.width, height: this.ctx.canvas.height};
+		this.channel.addEventListener('packet', (e:PacketRecievedEvent)=>{
+			if (e.packet.getTypeCode() === PacketTypeCode.STREAM_FRAME)
+				this._renderFrameMJPEG(e.packet);
+		});
+	}
+	_encodeFrame(data : Uint8Array) : string {
+		//Thanks to stackoverflow.com/a/11092371/2759984, stackoverflow.com/a/9458996/2759984
+		var binary : string = '';
+		for (var i = 0; i < data.length; i++) {
+			binary += String.fromCharCode(data[i]);
+		}
+		var output = window.btoa(binary);
+		var url = "data:image/jpeg;base64," + output;
+		return url;
+	}
+	_renderFrameMJPEG(packet : DataPacket) : void {
+		var image = new Image();
+		image.onload = e => this.ctx.drawImage(image, this.rect.top, this.rect.left);
+		image.src = this._encodeFrame(new Uint8Array(packet.getArrayBuffer(), 12));
+	}
 }
