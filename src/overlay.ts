2e58ab50160727aed590fd85efa582f4a6cb853a
mailmindlin <liamttlyrox@gmail.com>
2017-03-22 23:25:57 -0400

    Support more fun crosshair shapes!

--- src/overlay.ts
+++ src/overlay.ts
@@ -5,11 +5,24 @@ import {VideoStreamRenderer} from "./video";
 
 var colors : string[] = ['#F00','#0F0','#00F','#FF0','#F0F','#0FF','purple','orange','pink','grey','chartreuse'];
 
+enum CrosshairType {
+	INVISIBLE,
+	PLUS,
+	JOSH_S_EYE,
+	PENTAGON,
+	length
+}
+
+interface Crosshair extends Rectangle {
+	color : number;
+	type : CrosshairType;
+}
+
 export class OverlayRenderer extends Emitter implements VideoStreamRenderer {
 	protected state : RendererState = RendererState.STOPPED;
 	protected readonly ctx : CanvasRenderingContext2D;
 	protected bounds : Rectangle;
-	protected crosshairs : Rectangle[] = [];
+	protected crosshairs : Crosshair[] = [];
 	protected lastFoundRects : Rectangle[] = [];
 	protected OverlayMessage : protobuf.Type;
 	protected selectedOverlay : number = -1;
@@ -92,13 +105,16 @@ export class OverlayRenderer extends Emitter implements VideoStreamRenderer {
 		this.dispatchEvent(new CustomEvent('renderer.clobber', {detail:{renderer:this,rect:clobbered}}));
 	}
 	protected reset() : void {
-		this.crosshairs = [{top:.5,left:.5,width:.1,height:.1},{top:.5,left:.5,width:.1,height:.1}];
+		this.crosshairs = [];
+		for (var i = 0; i < 5; i++)
+			this.crosshairs.push({top:.5,left:.5,width:.1,height:.1,type:i % CrosshairType.length,color:i});
 	}
 	
 	protected __keypressHandler(e : KeyboardEvent) {
 		if (this.selectedOverlay < 0 && e.key != 'l')//Locked
 			return;
 		var dx = 0, dy = 0, dw = 0, dh = 0;
+		var dt = 0;
 		switch (e.key) {
 			case 'l':
 				//Lock/unlock
@@ -128,6 +144,17 @@ export class OverlayRenderer extends Emitter implements VideoStreamRenderer {
 			case 'a':
 				dw--;
 				break;
+			case 'e': 
+				dt++;
+				break;
+			case 'q':
+				dt--;
+				break;
+			case 'c': {
+				var crosshair = this.crosshairs[this.selectedOverlay - 1];
+				crosshair.color = ((crosshair.color || 0) + 1) % colors.length;
+				break;
+			}
 			case 'Tab':
 				this.selectedOverlay = (this.selectedOverlay % this.crosshairs.length) + 1;
 				break;
@@ -152,6 +179,14 @@ export class OverlayRenderer extends Emitter implements VideoStreamRenderer {
 		dh *= step * 1/this.bounds.height;
 		if (this.selectedOverlay > 0) {
 			var crosshair = this.crosshairs[this.selectedOverlay - 1];
+			
+			var type = (crosshair.type || 0) + dt;
+			if (type < 0)
+				type = CrosshairType.length - 1;
+			else if (type >= CrosshairType.length)
+				type = 0;
+			crosshair.type = type;
+			
 			crosshair.left += dx;
 			crosshair.top += dy;
 			crosshair.width += dw;
@@ -184,7 +219,16 @@ export class OverlayRenderer extends Emitter implements VideoStreamRenderer {
 	protected saveState() : void {
 		localStorage['crosshairs'] = JSON.stringify(this.crosshairs);
 	}
-
+	
+	protected drawPoly(ctx : CanvasRenderingContext2D, x : number, y : number, w : number, h : number, pts : number[][]) {
+		ctx.beginPath();
+		ctx.moveTo(x + 2 * w * (pts[0][0] - .5), y + 2 * h * (pts[0][1] - .5));
+		for (var i = 0; i < pts.length; i++)
+			ctx.lineTo(x + 2 * w * (pts[i][0] - .5), y + 2 * h * (pts[i][1] - .5));
+		ctx.closePath();
+		ctx.stroke();
+	}
+	
 	refresh() : Rectangle | null {
 		if (this.state != RendererState.RUNNING)
 			return null;
@@ -194,24 +238,40 @@ export class OverlayRenderer extends Emitter implements VideoStreamRenderer {
 		ctx.setTransform(bounds.width, 0, 0, bounds.height, bounds.left, bounds.top);
 		for (var i = 0; i < this.crosshairs.length; i++) {
 			var crosshair = this.crosshairs[i];
-			ctx.strokeStyle = colors[i % colors.length];
-			
+			ctx.strokeStyle = colors[crosshair.color];
 			var x = crosshair.left;
 			var y = crosshair.top;
 			var w = crosshair.width;
 			var h = crosshair.height;
 			
-			ctx.beginPath();
-			ctx.lineWidth = 1/bounds.width;
-			ctx.moveTo(x, y - h);
-			ctx.lineTo(x, y + h);
-			ctx.stroke();
-			
-			ctx.beginPath();
-			ctx.lineWidth = 1/bounds.height;
-			ctx.moveTo(x - w, y);
-			ctx.lineTo(x + w, y);
-			ctx.stroke();
+			switch (crosshair.type) {
+				case CrosshairType.INVISIBLE:
+					break;
+				case CrosshairType.PLUS:
+					ctx.beginPath();
+					ctx.lineWidth = 2/bounds.width;
+					ctx.moveTo(x, y - h);
+					ctx.lineTo(x, y + h);
+					ctx.stroke();
+					
+					ctx.beginPath();
+					ctx.lineWidth = 2/bounds.height;
+					ctx.moveTo(x - w, y);
+					ctx.lineTo(x + w, y);
+					ctx.stroke();
+					break;
+				case CrosshairType.JOSH_S_EYE:
+					ctx.lineWidth = 2/bounds.width;
+					var coords = [[0,0],[1,0],[1,.2],[.7,.2],[.7,.8],[1,.8],[1,1],[0,1],[0,.8],[.3,.8],[.3,.2],[0,.2]];
+					this.drawPoly(ctx, x, y, w, h, coords);
+					break;
+				case CrosshairType.PENTAGON: {
+					ctx.lineWidth = 2/bounds.width;
+					var coords = [[.5,0],[1,.4],[.75,1],[.25,1],[0,.4]];
+					this.drawPoly(ctx, x, y, w, h, coords);
+					break;
+				}
+			}
 			
 			//This crosshair is selected, so draw an ellipse around it
 			if (this.selectedOverlay - 1 == i) {
